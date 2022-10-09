832a7a7e168648b9f147507bdfc7e3ccea553229
farhan sharif <farhansharif1410@gmail.com>
2022-10-09 12:53:30 +0530

    some code adde

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart, removeClicked }) => {
+    let massage;
+    if (cart.length === 0) {
+        massage = <p>Please Buy One Item!!!</p>
+    } else if (cart.length === 1) {
+        massage = <div>
+            <h4>Amar Jonno Ekta Nao.</h4>
+            <h5>Tomar Jonn Ekta Nao.</h5>
+        </div>
+    }
+    else {
+        massage = <div>Thanks for Bye Item!!</div>
+    }
     return (
         <div>
             <h1>Cart Summary</h1>
@@ -15,6 +27,9 @@ const Cart = ({ cart, removeClicked }) => {
                     </p>
                     )
 
+                }
+                {
+                    massage
                 }
             </div>
         </div >
